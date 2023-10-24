e06f8ac1fa04aac14b87a57b3ab0f449261ffe68
MexlTuba <mexldelvertuba@gmail.com>
2023-10-24 21:07:22 +0800

    final final

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_doctor_app/models/doctor.dart';
-import 'package:flutter_doctor_app/screens/doctor_details.dart';
-import 'models/doctor.dart';
 import 'screens/home_page.dart';
-
-import 'package:flutter_doctor_app/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
